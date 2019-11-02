28d38d82457a077852f908d5e087ea84703cd9eb
rafaeldmpacheco <rafaeldmpacheco@gmail.com>
2019-11-02 18:07:36 -0300

    refactor modules

--- src/app/core/menu/menu.ts
+++ src/app/core/menu/menu.ts
@@ -1,60 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { LoginService } from '../../shared/providers/login.service';
-import { ModulesService } from '../../shared/providers/modules.service';
-import { ModulesPage } from '../../modules/settings/modules/modules';
-import { ProfilePage } from '../../modules/profile/profile';
 import { PlanPage } from '../../modules/boards/plan/plan';
+import { ProfilePage } from '../../modules/profile/profile';
+import { ModulesPage } from '../../modules/settings/modules/modules';
+import { ModulesService } from '../../shared/providers/modules.service';
 
 @Component({
-	templateUrl: 'menu.html'
+  templateUrl: 'menu.html'
 })
 export class MenuPage {
-	allMenuItems: any;
+  allMenuItems: any;
 
-	constructor(
-		private navController: NavController,
-		private modulesService: ModulesService,
-		private loginService: LoginService
-	) {
-		const moduleRegister = {
-			title: 'Modulos',
-			action: () => this.navController.push(ModulesPage),
-			icon: 'cog',
-			roles: ['ADMIN'],
-			isVisible: true,
-			name: 'modules'
-		};
+  constructor(private navController: NavController, private modulesService: ModulesService) {
+    const moduleRegister = {
+      title: 'Modulos',
+      action: () => this.navController.push(ModulesPage),
+      icon: 'cog',
+      roles: ['ADMIN'],
+      isVisible: true,
+      name: 'modules'
+    };
 
-		this.allMenuItems = JSON.parse(localStorage.getItem('modules'));
+    this.allMenuItems = JSON.parse(localStorage.getItem('modules'));
 
-		if (!this.allMenuItems) {
-			this.modulesService.getModules().subscribe(modules => {
-				this.allMenuItems = [...modules, moduleRegister];
+    if (!this.allMenuItems) {
+      this.modulesService.getModules().subscribe(modules => {
+        this.allMenuItems = [...modules, moduleRegister];
 
-				this.allMenuItems.forEach(element => {
-					this.setActions(element);
+        this.allMenuItems.forEach(element => {
+          this.setActions(element);
 
-					element.isVisible = true;
-				});
+          element.isVisible = true;
+        });
 
-				localStorage.setItem('modules', JSON.stringify(modules));
-			});
-		} else {
-			this.allMenuItems = [...this.allMenuItems, moduleRegister];
+        localStorage.setItem('modules', JSON.stringify(modules));
+      });
+    } else {
+      this.allMenuItems = [...this.allMenuItems, moduleRegister];
 
-			this.allMenuItems.forEach(element => {
-				this.setActions(element);
-			});
-		}
-	}
+      this.allMenuItems.forEach(element => {
+        this.setActions(element);
+      });
+    }
+  }
 
-	private setActions(element) {
-		if (element.name === 'plan') {
-			element.action = () => this.navController.push(PlanPage);
-		}
-		if (element.name === 'profile') {
-			element.action = () => this.navController.push(ProfilePage);
-		}
-	}
+  private setActions(element) {
+    if (element.name === 'plan') {
+      element.action = () => this.navController.push(PlanPage);
+    }
+    if (element.name === 'profile') {
+      element.action = () => this.navController.push(ProfilePage);
+    }
+  }
 }
